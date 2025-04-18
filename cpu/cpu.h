214ba8142c4b20c561b53bdd7f3116f01a5067ed
kacpereqo <remzak.k@gmail.com>
2025-04-18 23:36:42 +0200

    feat: instruction headers arguments

--- cpu/cpu.h
+++ cpu/cpu.h
@@ -15,6 +15,7 @@ enum class ProcessorStatus {
     InterruptDisable = 1 << 2,
     DecimalMode      = 1 << 3,
     BreakCommand     = 1 << 4,
+    // Unused        = 1 << 5, 7th bit is unused
     Overflow         = 1 << 6,
     Negative         = 1 << 7,
 };
@@ -39,12 +40,12 @@ class CPU {
     /// Instructions
 
     /// Load/Store
-    void LDA(); // Load Accumulator
-    void LDX(); // Load Index Register X
-    void LDY(); // Load Index Register Y
-    void STA(); // Store Accumulator
-    void STX(); // Store Index Register X
-    void STY(); // Store Index Register Y
+    void LDA(std::uint8_t value);    // Load Accumulator
+    void LDX(std::uint8_t value);    // Load Index Register X
+    void LDY(std::uint8_t value);    // Load Index Register Y
+    void STA(std::uint8_t value);    // Store Accumulator
+    void STX(std::uint16_t address); // Store Index Register X
+    void STY(std::uint16_t address); // Store Index Register Y
 
     ///Register Transfer
     void TAX(); // Transfer Accumulator to Index Register X
@@ -60,46 +61,46 @@ class CPU {
     void PLA(); // Pull Accumulator from Stack
 
     /// Logical
-    void AND(); // Logical AND
-    void ORA(); // Logical OR
-    void EOR(); // Logical XOR
-    void BIT(); // Bit Test
+    void AND(std::int8_t value); // Logical AND
+    void ORA(std::int8_t value); // Logical OR
+    void EOR(std::int8_t value); // Logical XOR
+    void BIT(std::int8_t value); // Bit Test
 
     /// Arithmetic
-    void ADC(); // Add with Carry
-    void SBC(); // Subtract with Carry
-    void CMP(); // Compare Accumulator
-    void CPX(); // Compare Index Register X
-    void CPY(); // Compare Index Register Y
+    void ADC(std::int8_t value); // Add with Carry
+    void SBC(std::int8_t value); // Subtract with Carry
+    void CMP(std::int8_t value); // Compare Accumulator
+    void CPX(std::int8_t value); // Compare Index Register X
+    void CPY(std::int8_t value); // Compare Index Register Y
 
     /// Increments & Decrements
-    void INC(); // Increment Memory
-    void INX(); // Increment Index Register X
-    void INY(); // Increment Index Register Y
-    void DEC(); // Decrement Memory
-    void DEX(); // Decrement Index Register X
-    void DEY(); // Decrement Index Register Y
+    void INC(std::int16_t address); // Increment Memory
+    void INX();                     // Increment Index Register X
+    void INY();                     // Increment Index Register Y
+    void DEC(std::int16_t address); // Decrement Memory
+    void DEX();                     // Decrement Index Register X
+    void DEY();                     // Decrement Index Register Y
 
     /// Shifts
-    void ASL(); // Arithmetic Shift Left
-    void LSR(); // Logical Shift Right
-    void ROL(); // Rotate Left
-    void ROR(); // Rotate Right
+    void ASL(std::int8_t value); // Arithmetic Shift Left
+    void LSR(std::int8_t value); // Logical Shift Right
+    void ROL(std::int8_t value); // Rotate Left
+    void ROR(std::int8_t value); // Rotate Right
 
     /// Jumps & Calls
-    void JMP(); // Jump to Address
-    void JSR(); // Jump to Subroutine
+    void JMP(std::uint16_t address); // Jump to Address
+    void JSR(std::uint16_t address); // Jump to Subroutine
     void RTS(); // Return from Subroutine
 
     /// Branches
-    void BCC(); // Branch if Equal
-    void BCS(); // Branch if Carry Set
-    void BEQ(); // Branch if Equal
-    void BMI(); // Branch if Minus
-    void BNE(); // Branch if Not Equal
-    void BPL(); // Branch if Positive
-    void BVC(); // Branch if Overflow Clear
-    void BVS(); // Branch if Overflow Set
+    void BCC(std::int8_t value); // Branch if Equal
+    void BCS(std::int8_t value)); // Branch if Carry Set
+    void BEQ(std::int8_t value)); // Branch if Equal
+    void BMI(std::int8_t value)); // Branch if Minus
+    void BNE(std::int8_t value)); // Branch if Not Equal
+    void BPL(std::int8_t value)); // Branch if Positive
+    void BVC(std::int8_t value)); // Branch if Overflow Clear
+    void BVS(std::int8_t value)); // Branch if Overflow Set
 
     /// Control
     void CLC(); // Clear Carry Flag
---
+++ cpu/instructions/load.cpp
@@ -0,0 +1,4 @@
+//
+// Created by thinkPaździerż on 18.04.2025.
+//
+`
